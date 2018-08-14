28e73842ee577cac50b450e94a3d75db685f17fc
crea <masataka0504@gmail.com>
2018-08-14 22:23:21 +0900

    1st commit

---
+++ GoddammitFramework/GoddammitFramework.h
@@ -0,0 +1,19 @@
+//
+//  GoddammitFramework.h
+//  GoddammitFramework
+//
+//  Created by masa on 2018-08-14.
+//  Copyright © 2018 masa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoddammitFramework.
+FOUNDATION_EXPORT double GoddammitFrameworkVersionNumber;
+
+//! Project version string for GoddammitFramework.
+FOUNDATION_EXPORT const unsigned char GoddammitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoddammitFramework/PublicHeader.h>
+
+
---
+++ GoddammitFramework/Goddemnit.swift
@@ -0,0 +1,16 @@
+//
+//  Goddemnit.swift
+//  GoddammitFramework
+//
+//  Created by masa on 2018-08-14.
+//  Copyright © 2018 masa. All rights reserved.
+//
+
+import Foundation
+
+public struct Goddemnit {
+    public init(){}
+    public func curse() {
+        print("Goddemmit...!")
+    }
+}
